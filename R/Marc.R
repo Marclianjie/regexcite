e026d9a375e6439fb57b3cdb50e565b6a0cb4e15
Marc Shi <lianjieshi02@gmail.com>
2024-07-31 22:47:58 -0700

    Initial commit

--- R/Marc.R
+++ R/Marc.R
@@ -1,7 +1,7 @@
 library(devtools)
 packageVersion("devtools")
 use_git()
-1#> ✔ Initialising Git repo.
+#> ✔ Initialising Git repo.
 #> ✔ Adding ".Rhistory", ".Rdata", ".httr-oauth", ".DS_Store", and
 #>   ".quarto" to '.gitignore'.
 
