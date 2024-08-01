e3bc8f261df96efb58982a79bc08440e3bb2045d
Marc Shi <lianjieshi02@gmail.com>
2024-07-31 22:35:00 -0700

    Initial commit

--- R/Marc.R
+++ R/Marc.R
@@ -4,3 +4,22 @@ use_git()
 1#> ✔ Initialising Git repo.
 #> ✔ Adding ".Rhistory", ".Rdata", ".httr-oauth", ".DS_Store", and
 #>   ".quarto" to '.gitignore'.
+
+(x <- "alfa,bravo,charlie,delta")
+#> [1] "alfa,bravo,charlie,delta"
+strsplit(x, split = ",")
+#> [[1]]
+#> [1] "alfa"    "bravo"   "charlie" "delta"
+str(strsplit(x, split = ","))
+#> List of 1
+#>  $ : chr [1:4] "alfa" "bravo" "charlie" "delta"
+
+strsplit(x, split = ",")[[1]]
+strsplit1 <- function(x, split) {
+  strsplit(x, split = split)[[1]]
+}
+use_r("strsplit1")
+#> ☐ Edit 'R/strsplit1.R'.
+#>
+load_all()
+#> ℹ Loading regexcite
